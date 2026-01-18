59ef61c0e8e87f15dc4357d29a0059cf25ec2ec6
Nguyen Van Dat <vandatnguyen919@gmail.com>
2026-01-18 13:52:17 +0700

    app bar

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -19,7 +19,7 @@ function ModeSelect() {
   }
 
   return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+    <FormControl size="small" sx={{ minWidth: 120 }}>
       <InputLabel id="demo-select-small-label">Mode</InputLabel>
       <Select
         labelId="demo-select-small-label"
--- src/theme.js
+++ src/theme.js
@@ -9,16 +9,68 @@ const theme = createTheme({
     colorSchemeSelector: 'class'
   },
   colorSchemes: {
-    light: {
-      palette: {
-        primary: { main: '#1976d2' },
-        secondary : { main: '#dc004e' }
+    // light: {
+    //   palette: {
+    //     primary: { main: '#1976d2' },
+    //     secondary : { main: '#dc004e' }
+    //   }
+    // },
+    // dark: {
+    //   palette: {
+    //     primary: { main: '#90caf9' },
+    //     secondary : { main: '#f48fb1' }
+    //   }
+    // }
+    light: true, dark: true
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#90caf9'
+          }
+        }
       }
     },
-    dark: {
-      palette: {
-        primary: { main: '#90caf9' },
-        secondary : { main: '#f48fb1' }
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem',
+          '& .MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.light
+          },
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.main
+          },
+          '& fieldset': {
+            borderWidth: '1px !important'
+          }
+        })
       }
     }
   }
